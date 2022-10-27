74e2a8cf04e104a5c3aaf8eb2813738aed29c286
Abdullah Al Mamun <abdullah.mamun2528@gmail.com>
2022-10-27 22:14:13 +0600

    Pattern updated successfully

--- pattern.c
+++ pattern.c
@@ -1,25 +1,30 @@
 #include <stdio.h>
 
-int main() {
-    
+int main()
+{
+
     int N;
     // Input N form User
+
     scanf("%d", &N);
-    
-    for(int i=0; i<N; i++){
-        for(int j =0; j<=i; j++){
+
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = 0; j <= i; j++)
+        {
             printf("*");
         }
         printf("\n");
     }
-    
-    for(int i=N-1; i>0; i--){
-        for(int j =0; j<i; j++){
+
+    for (int i = N - 1; i > 0; i--)
+    {
+        for (int j = 0; j < i; j++)
+        {
             printf("*");
         }
         printf("\n");
     }
-    
 
     return 0;
 }
