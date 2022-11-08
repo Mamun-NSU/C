6048b2d93a7c468b178bd34531af16ec14c49f6a
Abdullah Al Mamun <abdullah.mamun2528@gmail.com>
2022-11-09 02:46:13 +0600

    add&mult solved successfully

---
+++ add&mult.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main()
+{
+
+    int N, K, sum = 1;
+    scanf("%d %d", &N, &K);
+
+    //   printf("%d %d \n",N, K);
+
+    for (int i = 0; i < N; i++)
+    {
+
+        if (sum < K)
+        {
+            sum = sum * 2;
+        }
+        else
+        {
+            sum = sum + K;
+        }
+        //   printf("%d %d \n",sum, i);
+    }
+
+    printf("%d\n", sum);
+
+    return 0;
+}
