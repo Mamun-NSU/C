a666ead19baccd78194e48ae6db64734207bb16b
Abdullah Al Mamun <abdullah.mamun2528@gmail.com>
2022-11-09 02:16:09 +0600

    loveA solved successfully

---
+++ loveA.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    int count = 0, m = 0;
+
+    char str[100];
+    fgets(str, 100, stdin);
+
+    int n = strlen(str) - 1;
+    // printf("%d", n);
+
+    for (int i = 0; i < n; i++)
+    {
+        if (str[i] == 'a')
+            count++;
+    }
+    // printf("%d", count);
+
+    m = n / 2;
+    if (count > m)
+    {
+        printf("%d\n", n);
+    }
+    else
+    {
+        printf("%d\n", count * 2 - 1);
+    }
+
+    return 0;
+}
