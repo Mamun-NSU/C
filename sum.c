10540477240c720d2dc0da2b05ff82114f28c825
Abdullah Al Mamun <abdullah.mamun2528@gmail.com>
2023-04-14 23:59:43 +0600

    705A - Hulk solved successfully

---
+++ sum.c
@@ -0,0 +1,30 @@
+
+#include <stdio.h>
+
+int main()
+{
+    int N;
+    scanf("%d", &N);
+
+    int A[N];
+    long long int sum = 0;
+
+    for (int i = 0; i < N; i++)
+    {
+        scanf("%d", &A[i]);
+
+        sum = sum + A[i];
+    }
+
+    if (sum < 0)
+    {
+        sum = sum * -1;
+        printf("%lld ", sum);
+    }
+    else
+    {
+        printf("%lld ", sum);
+    }
+
+    return 0;
+}
