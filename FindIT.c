37fd15ec78e84dfede2690f6b75b50cd51fb816d
Abdullah Al Mamun <abdullah.mamun2528@gmail.com>
2023-04-26 02:12:45 +0600

    Find it solved successfully

---
+++ FindIT.c
@@ -0,0 +1,30 @@
+
+#include <stdio.h>
+
+int main()
+{
+
+    int N, count = 0;
+    scanf("%d", &N);
+
+    int A[N];
+
+    for (int i = 0; i < N; i++)
+    {
+        scanf("%d", &A[i]);
+    }
+
+    int X;
+    scanf("%d", &X);
+
+    for (int i = 0; i < N; i++)
+    {
+        if (A[i] == X)
+        {
+            count++;
+        }
+    }
+
+    printf("%d \n", count);
+    return 0;
+}
