8b8361d43624dc4ba98db0aebc9cfa9a0622e2db
Abdullah Al Mamun <abdullah.mamun2528@gmail.com>
2023-04-28 17:38:05 +0600

    count2  solved successfully

---
+++ count3.c
@@ -0,0 +1,32 @@
+
+
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+
+    char S[1000];
+    scanf("%s", S);
+
+    int A[26] = {0};
+    int i = 0;
+    int newIndex;
+    while (S[i] != '\0')
+    {
+
+        newIndex = S[i] - 'a';
+        A[newIndex]++;
+
+        i++;
+    }
+
+    char C = 'a';
+    for (int i = 0; i < 26; i++)
+    {
+        printf("%c - %d \n", C, A[i]);
+        C++;
+    }
+
+    return 0;
+}
