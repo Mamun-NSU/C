0af1586a046f4c1a912ccd6fe74f08fcd0c8a16b
Abdullah Al Mamun <abdullah.mamun2528@gmail.com>
2023-05-03 23:50:30 +0600

    updated successfully

--- Calculator.c
+++ Calculator.c
@@ -5,5 +5,38 @@ int main()
     printf("Hello World");
     printf("This is test purpose.");
 
+    int A, B;
+
+    scanf("%d %d", &A, &B);
+
+    char ch;
+
+    scanf("%c", &ch);
+
+    int result;
+
+    if (ch == "+")
+    {
+        result = A + B;
+    }
+    else if (ch == "-")
+    {
+        result = A - B;
+    }
+    else if (ch == "/")
+    {
+        result = A / B;
+    }
+    else if (ch == "*")
+    {
+        result = A * B;
+    }
+    else
+    {
+        result = 0;
+    }
+
+    printf("%d", result);
+
     return 0;
 }
