8377105abbe1766185a3c2081bf5683a9bcd6cea
Abdullah Al Mamun <abdullah.mamun2528@gmail.com>
2023-05-19 10:59:50 +0600

    Millde Man solved successfully

---
+++ middleMan.c
@@ -0,0 +1,51 @@
+
+
+#include <stdio.h>
+
+int main()
+{
+    int N;
+    scanf("%d", &N);
+
+    int A[N];
+    int temp;
+
+    for (int i = 0; i < N; i++)
+    {
+        scanf("%d", &A[i]);
+    }
+
+    // printf("\nBefore sorting: \n");
+    // for(int i=0; i<N; i++){
+    //     printf("%d ", A[i]);
+    // }
+
+    for (int i = 0; i < N - 1; i++)
+    {
+        for (int j = i + 1; j < N; j++)
+        {
+            if (A[i] > A[j])
+            {
+                temp = A[i];
+                A[i] = A[j];
+                A[j] = temp;
+            }
+        }
+    }
+
+    // printf("\nAfter sorting: \n");
+    // for(int i=0; i<N; i++){
+    //     printf("%d ", A[i]);
+    // }
+    // printf("\n");
+
+    if (N % 2 == 1)
+    {
+        printf("%d\n", A[N / 2]);
+    }
+    else
+    {
+        printf("%d %d\n", A[N / 2 - 1], A[N / 2]);
+    }
+    return 0;
+}
