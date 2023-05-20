e509601178c910601962d2ab8ca6a6ce4b89374e
Abdullah Al Mamun <abdullah.mamun2528@gmail.com>
2023-05-20 23:45:41 +0600

    Pattern solved successfully

---
+++ nicePattern.c
@@ -0,0 +1,62 @@
+
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main()
+{
+
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+    int N, S, K;
+    scanf("%d", &N);
+    S = 1, K = N - 1;
+
+    for (int i = 1; i <= N; i++)
+    {
+        for (int j = 1; j <= K; j++)
+        {
+            printf(" ");
+        }
+        for (int j = 1; j <= S; j++)
+        {
+            if (i % 2 == 1)
+            {
+                printf(">");
+            }
+            else
+            {
+                printf("<");
+            }
+        }
+        K--;
+        S = S + 2;
+        printf("\n");
+    }
+
+    // Thi is for lower side
+    K = N - 1;
+    for (int i = N - 1; i >= 1; i--)
+    {
+        for (int j = K; j < N; j++)
+        {
+            printf(" ");
+        }
+
+        for (int j = 1; j <= 2 * i - 1; j++)
+        {
+            if (i % 2 == 1)
+            {
+                printf(">");
+            }
+            else
+            {
+                printf("<");
+            }
+        }
+        printf("\n");
+        K--;
+    }
+
+    return 0;
+}
